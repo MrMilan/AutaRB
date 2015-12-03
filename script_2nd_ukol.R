9d0a750a695be8364a812c1f9f55262eed77ade2
Milan Polacek <polacek.emilan@gmail.com>
2015-12-03 02:31:35 +0100

    Finalni skript A zprava pred opravou editorem

--- script_2nd_ukol.R
+++ script_2nd_ukol.R
@@ -1,7 +1,12 @@
 
 rm(list=ls()) #mazani promenych
 cat("\014")  #mazani konzole
-cars <- read.csv("~/Schule/FEL/21rocnik/DVZ/Auta2/cars.csv")
+carsa <- read.csv("~/Scholla/___FEL/21rocnik/DVZ/AutaRB/cars.csv")
+
+cars <- carsa
+
+cars <- na.omit(cars) #odstraneni nulovych
+cars.label <- cars
 
 cars$Manufacturer <- as.numeric(cars$Manufacturer)
 cars$Model <- as.numeric(cars$Model)
@@ -31,7 +36,46 @@ cars$Weight <- as.numeric(cars$Weight)
 cars$Origin <- as.numeric(cars$Origin)
 
 
-cars <- na.omit(cars) #odstraneni nulovych
+
+
+cars.use = cars[,-c(1,2,4,6)]
+medians = apply(cars.use,2,median)
+mads = apply(cars.use,2,mad)
+cars.use1 <- cars.use
+cars.use = scale(cars.use,center=medians,scale=mads)
+
+cars.dist = dist(cars.use)
+cars.dist[is.infinite(cars.dist)] <- 100 #odstraneni nulovych
+cars.hclust = hclust(cars.dist)
+plot(cars.hclust,labels=cars.label$Model,main='Complete from hclust')#vykresleni
+
+cars.wardClust <- hclust(cars.dist,method = "ward.D")#dobrz pro blizke skupiny
+plot(cars.wardClust,labels=cars.label$Model,main='Ward.D from hclust')
+
+#deleni do skupin
+cars.cutToGroup.ward <- cutree(cars.wardClust, 3)
+cars.cutToGroup.complete <- cutree(cars.hclust, 4)
+
+#prumerovani
+a3 = aggregate(cars[,-c(1,2)],list(cars.cutToGroup.ward),median)
+a4 = aggregate(cars[,-c(1,2)],list(cars.cutToGroup.complete),median)
+
+#vloyeni do tabulkz a export
+tab3<- data.frame(Cluster=a3[,1],Freq=as.vector(table(cars.cutToGroup.ward)),a3[,-1])
+tab4<- data.frame(Cluster=a4[,1],Freq=as.vector(table(cars.cutToGroup.complete)),a4[,-1])
+
+write.table(tab3, file = "~/Scholla/___FEL/21rocnik/DVZ/AutaRB/tab3.csv", sep = ";", col.names = NA,qmethod = "double")
+write.table(tab4, file = "~/Scholla/___FEL/21rocnik/DVZ/AutaRB/tab4.csv", sep = ";", col.names = NA,qmethod = "double")
+
+#vzrobci aut
+aa3 = aggregate(cars,list(cars.cutToGroup.ward),median)
+aa4 = aggregate(cars,list(cars.cutToGroup.complete),median)
+
+taab3<- data.frame(Cluster=a3[,1],Freq=as.vector(table(cars.cutToGroup.ward)),a3[,-1])
+taab4<- data.frame(Cluster=a4[,1],Freq=as.vector(table(cars.cutToGroup.complete)),a4[,-1])
+
+
+
 
 cars <- scale(cars) #normalizace
 
